bf80e685ee4b79d0f670b062492a965dd6e39c88
James Rungsawang <jamesrngsawang@gmail.com>
2020-09-25 00:24:58 -0700

    Fixed author solution

--- NWERC_2019/F_Firetrucks_Are_Red/Author_Solution/main.cpp
+++ NWERC_2019/F_Firetrucks_Are_Red/Author_Solution/main.cpp
@@ -6,21 +6,17 @@ using namespace std;
 struct UnionFind {
 	vector<int> par, rank, size; int c;
 	UnionFind(int n) : par(n), rank(n,0), size(n,1), c(n) {
-		for (int i = 0; i < n; ++i) par[i] = i;
+        for (int i = 0; i < n; ++i) par[i] = i;
 	}
-	int find(int i) { return (par[i] == i ? i : (par[i] = find(par[i]))); }
-	bool same(int i, int j) { return find(i) == find(j); }
-	int get_size(int i) { return size[find(i)]; }
-	int count() { return c; }
-	int merge(int i, int j) {
-		if ((i = find(i)) == (j = find(j))) return -1; else --c;
+    int find(int i) { return (par[i] == i ? i : (par[i] = find(par[i]))); }
+    bool same(int i, int j) { return find(i) == find(j); }
+    int get_size(int i) { return size[find(i)]; }
+    int count() { return c; }
+    int merge(int i, int j) {
         i = find(i), j = find(j);
         if(i == j)
             return -1;
-		//if (rank[i] > rank[j]) swap(i, j);
-		par[i] = j;
-		//if (rank[i] == rank[j]) rank[j]++;
-		//return j;
+        par[i] = j;
 	}
 };
 
@@ -33,9 +29,9 @@ int main(){
 	map<int, int> root;
 	vector<S> ans;
 	for(int i = 0; i < n; ++i){
-		int m; cin >> m;
-		while(m--){
-		int x; cin >> x;
+        int m; cin >> m;
+        while(m--){
+            int x; cin >> x;
             int r;
             if(root.count(x)){
                 r = root[x];
@@ -52,7 +48,7 @@ int main(){
 		}
 	}
     
-	if(ans.size() == n-1)
-		for(auto p : ans) cout << p.i+1 << ' ' << p.j+1 << ' ' << p.n << endl;
-	else cout << "impossible" << endl;
+    if(ans.size() == n-1)
+        for(auto p : ans) cout << p.i+1 << ' ' << p.j+1 << ' ' << p.n << endl;
+    else cout << "impossible" << endl;
 }
