62042bfe55ff619b6bdeb1260775a25a0c29d19f
voidcs <jamesrngsawang@gmail.com>
2020-12-31 02:29:06 -0800

    NWERC 2017 Boss battle solution

---
+++ NWERC_2017/B_Boss_Battle/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll INF = 1e18;
+ 
+int main(){
+    int n;
+    cin>>n;
+    if(n <= 3)
+        cout<<"1\n";
+    else{
+        cout<<n-2<<endl;
+    }
+    return 0;
+}
