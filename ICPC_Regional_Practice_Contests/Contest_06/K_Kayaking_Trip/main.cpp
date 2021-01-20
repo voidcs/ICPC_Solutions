35073934b289e929904c1e036a9e03f822f942d4
voidcs <jamesrngsawang@gmail.com>
2021-01-19 21:46:02 -0800

    ICPC regional practice contest #6 problem K solved with binary search

---
+++ ICPC_Regional_Practice_Contests/Contest_06/K_Kayaking_Trip/main.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll INF = 1e18;
+
+int main(){
+    vector<ll> freq(3), str(3);
+    ll n = 0;
+    for(int i = 0; i < 3; i++){
+        cin>>freq[i];
+        n += freq[i];
+    }
+    for(int i = 0; i < 3; i++)
+        cin>>str[i];
+    n /= 2;
+    ll speeds[n];
+    for(int i = 0; i < n; i++)
+        cin>>speeds[i];
+    vector<array<ll, 2>> types;
+    for(int i = 0; i < 3; i++)
+        for(int j = 0; j <= i; j++)
+            types.push_back({i, j});
+      
+    function<bool(array<ll, 2>, array<ll, 2>)> comp = [&](array<ll, 2> a, array<ll, 2> b){
+        return (str[a[0]]+str[a[1]] < str[b[0]]+str[b[1]]);
+    };
+    sort(speeds, speeds + n);
+    sort(types.begin(), types.end(), comp);
+    
+    function<bool(vector<ll>, ll)> valid = [&](vector<ll> f, ll minimum){
+        ll i = n-1;
+        for(auto x: types){
+            ll sum = str[x[0]] + str[x[1]];
+            while(i >= 0 && f[x[0]] >= 1 + (x[0] == x[1]) && f[x[1]] >= 1 && sum*speeds[i] >= minimum){
+                i--;
+                f[x[0]]--;
+                f[x[1]]--;
+            }
+        }
+        return i == -1;
+    };
+    ll ans = 0;
+    ll l = 0, r = 1e10;
+    while(l <= r){
+        ll mid = (l+r)/2;
+        if(valid(freq, mid)){
+            l = mid+1;
+            ans = mid;
+        }
+        else
+            r = mid-1;
+    }
+    cout<<ans<<endl;
+    
+    return 0;
+}
